e57c14800d4ebd1db009d109f0f860c300be59fc
gammut <gammut20@gmail.com>
2021-08-24 22:03:01 -0500

    Creacion de proyectos de curso

---
+++ curso-hilos/src/main/java/com/taller/curso04/Competidor.java
@@ -0,0 +1,53 @@
+package com.taller.curso04;
+
+/*POJO
+ * Plain old java Object
+ */
+
+
+public class Competidor extends Thread{
+
+	private String nombre;
+	private String codigo;
+	
+	
+	public Competidor(String nombre, String codigo) {
+		super();
+		this.nombre = nombre;
+		this.codigo = codigo;
+	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+	
+	@Override
+	public void run() {
+		
+		for(int i= 0; i < 10; i++) {
+			
+			System.out.println("Competidor " + this.nombre + "numero: " + this.codigo);
+		}
+		
+	}
+	
+	
+	
+
+}
---
+++ curso-hilos/src/main/java/com/taller/curso04/Principal.java
@@ -0,0 +1,30 @@
+package com.taller.curso04;
+
+import java.util.Date;
+
+public class Principal {
+	
+	public static void main(String[] args) {
+		
+		Competidor competidor1 = new Competidor("Hinojosa", "0005");
+		Competidor competidor2 = new Competidor("Briones", "0006");
+		Competidor competidor3 = new Competidor("Kelvincito", "0666");
+
+		//start
+		
+		System.out.println("Inicio de competencia" + new Date());
+		
+		competidor1.setPriority(1);
+		competidor2.setPriority(10);
+		competidor3.setPriority(5);
+		
+		
+		competidor1.start();
+		competidor2.start();
+		competidor3.start();
+		
+		
+		System.out.println("Fin de la competencia" + new Date());
+	}
+
+}
